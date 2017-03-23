b0a8cf02b694602aca6297826ddcc87f9c5c64d3
fabianlundgren <fabbe.lundgren@gmail.com>
2017-03-23 21:24:50 +0100

    Made changes in the application.rb file

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,14 @@ require "action_cable/engine"
 Bundler.require(*Rails.groups)
 module CooperApi
   class Application < Rails::Application
-      config.middleware.insert_before 0, Rack::Cors do
-        allow do
-          origins '*'
-          resource '*', headers: :any, methods: [:get, :post, :put, :delete]
-        end
-      end
-      config.api_only = true
-    end
-  end
+    config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :head],
+                  expose: %w(access-token expiry token-type uid client),
+                  max_age: 0
+                end
+              end
+            end
+          end
+          
